46140bcdafa76994b1329bab3e070d6a51d88512
VN\dung.phan <dung.phan@vn.defide.co.jp>
2019-03-13 16:01:02 +0700

    #react-event-two-way bidning

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  state = {
+    name: 'TechSmith'
+  }
+
+  changeName = (newName) => {
+    this.setState({
+      name: newName
+    });
+  }
+
+  changeNameFromInput = (e) => {
+    this.setState({
+      name: e.target.value
+    });
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <br /> <br />
+        <button onClick={() => this.changeName('Awesome TechSmith')}>Change State Using Anon Funtion</button>
+        <button onClick={this.changeName.bind(this, 'Awesome TechSmith -----')}>Change State Using Anon Funtion</button>
+        <br /> <br />
+        <input type="text" onChange={this.changeNameFromInput} value={this.state.name} />
+        <br /> <br />
+        <div>{this.state.name}</div>
+        
       </div>
     );
   }
