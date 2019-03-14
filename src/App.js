0be26a6bce4d54ececd357d6fba8c2325156efb5
VN\dung.phan <dung.phan@vn.defide.co.jp>
2019-03-14 15:26:06 +0700

    return just an array [,]

--- src/App.js
+++ src/App.js
@@ -1,61 +1,24 @@
 import React, { Component } from 'react';
-import UniqueId from 'react-html-id';
+// import UniqueId from 'react-html-id';
 
 import './App.css';
 
-import User from './User';
+const Temp = () => {
+  return (
+    [
+      <div key="1">Hi</div>,
+      <div key="2">Hello</div>
+    ]
+  );
+}
 
 
 class App extends Component {
 
-  constructor() {
-    super();
-    UniqueId.enableUniqueIds(this);
-    this.state = {
-      users: [
-        { id: this.nextUniqueId(), name: 'John', age: 20 },
-        { id: this.nextUniqueId(), name: 'Peter', age: 30 },
-        { id: this.nextUniqueId(), name: 'Alan', age: 50 },
-      ]
-    }
-
-    console.log(this.state);
-
-  }
-
-  deleteUser = (index, e) => {
-    const users = Object.assign([], this.state.users);
-    users.splice(index, 1);
-    this.setState({
-      users
-    });
-  }
-
-  changeUserName = (userId, e) => {
-    const index = this.state.users.findIndex((user)=> {
-      return user.id === userId;
-    });
-
-    const user = Object.assign({}, this.state.users[index]);
-
-    user.name = e.target.value;
-
-    const users = Object.assign([], this.state.users);
-
-    users[index] = user;
-
-    this.setState({users});
-  }
-
   render() {
     return (
       <div className="App">
-        <ul>
-          {this.state.users.map((user, index) => {
-            return <User key={user.id} name={user.name} age={user.age} delEvent={this.deleteUser.bind(this, index)} 
-            changeEvent={this.changeUserName.bind(this, user.id)} />
-          })}
-        </ul>
+        <Temp />
       </div>
     );
   }
