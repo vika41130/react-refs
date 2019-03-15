6944bf715be25e227e42021f574430443cc01ae6
VN\dung.phan <dung.phan@vn.defide.co.jp>
2019-03-15 14:55:03 +0700

    PureComponent

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import React, { Component, Fragment } from 'react';
+import React, { Component, PureComponent } from 'react';
 
 import './App.css';
 
-const Temp = () => {
+const Temp = (props) => {
+  console.log("render temp");
   return (
-    <Fragment>
-      <div>Hi</div>
-      <div>Hello</div>
-    </Fragment>
+    <div>{props.test}</div>
   );
 }
 
+class App extends PureComponent {
 
-class App extends Component {
+  state = {
+    val: 1
+  }
+
+  componentDidMount() {
+    setInterval(() => {
+      this.setState(() => {
+        return { val: 2 }
+      });
+    }, 2000);
+  }
+
+
+  // componentDidMount() {
+  //   setInterval(() => {
+  //     this.setState(() => {
+  //       return { val: Math.random() }
+  //     });
+  //   }, 2000);
+  // }
+
+  // shouldComponentUpdate(nextProps, nextState) {
+  //   console.log("nextState", nextState);
+  //   console.log("current state", this.state);
+  //   return ( this.state === nextState ? false : true );
+  // }
 
   render() {
+    console.log("render App");
     return (
       <div className="App">
-        <Temp />
+        <Temp test={ this.state.val } />
       </div>
     );
   }
